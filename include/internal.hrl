f2d27064b374c05fb3cbda711d8eb2b22678b737
Stephane Bourque <stephane.bourque@gmail.com>
2020-10-15 09:34:12 -0700

    Original commit

---
+++ include/internal.hrl
@@ -0,0 +1,12 @@
+%%%-------------------------------------------------------------------
+%%% @author stephb
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 14. Oct 2020 11:34 p.m.
+%%%-------------------------------------------------------------------
+-author("stephb").
+
+
+-record(mqtt_processor_state, { listener_pid, parent_pid, peer_ip, connection_state }).

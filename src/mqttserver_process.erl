8a6cf5de5cf06d90023b2181111c7d116cc28ae7
Stephane Bourque <stephane.bourque@gmail.com>
2020-10-17 23:56:47 -0700

    Message decoding

--- src/mqttserver_process.erl
+++ src/mqttserver_process.erl
@@ -19,5 +19,10 @@ process(Data,State)->
 	io:format("Data (~p bytes)>>> ~p~n",[size(Data),Data]),
 	Msg = message:decode(Data),
 	io:format("Message: ~p~n",[Msg]),
-	{ <<"Hello from MQTT">>, State }.
+	answer_msg( Msg, State).
 
+answer_msg( Msg, State ) when is_record(Msg,mqtt_connect_variable_header) ->
+	VariableHeader = #mqtt_connack_variable_header{ connect_acknowledge_flag = 0,connect_reason_code = ?MQTT_RC_CONNECTION_ACCEPTED, properties = [] },
+	Msg = #mqtt_msg{ packet_type = ?MQTT_CONNACK , variable_header = VariableHeader },
+	Blob = message:encode(Msg),
+	{ Blob, State}.

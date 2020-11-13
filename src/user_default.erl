78bf2feae9b41fe01a97a1de400b2632dadb08cf
Stephane Bourque <stephane.bourque@gmail.com>
2020-11-12 21:46:55 -0800

    Adding some documentation

--- src/user_default.erl
+++ src/user_default.erl
@@ -15,18 +15,38 @@
 
 -define(SIM_APIKEY,sim_api_key).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%%  Node commands
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-spec login( ApiKey :: string()) -> ok.
 login(ApiKey)->
 	persistent_term:put(?SIM_APIKEY,ApiKey).
 
+-spec logout() -> ok.
 logout()->
 	persistent_term:erase(?SIM_APIKEY).
 
-refresh_ouis()->
-	oui_server:refresh().
-
+-spec connected() -> { ok , none } | { ok , Manager::node() }.
 connected()->
 	simnode:connected().
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%%  Manager commands
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-spec refresh_ouis()-> ok.
+refresh_ouis()->
+	oui_server:refresh().
+
+-spec connected_nodes() -> {ok,[node()]}.
 connected_nodes()->
 	manager:connected_nodes().
 

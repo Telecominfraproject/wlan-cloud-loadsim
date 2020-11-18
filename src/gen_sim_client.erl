d58947fab6f136380bb10cb678e8bb1734f7cbbf
Stephane Bourque <stephane.bourque@gmail.com>
2020-11-18 13:11:54 -0800

    Adding gen_sim_client

---
+++ src/gen_sim_client.erl
@@ -0,0 +1,19 @@
+%%%-------------------------------------------------------------------
+%%% @author stephb
+%%% @copyright (C) 2020, Arilia Wireless Inc.
+%%% @doc
+%%%
+%%% @end
+%%% Created : 17. Nov 2020 4:26 p.m.
+%%%-------------------------------------------------------------------
+-module(gen_sim_client).
+-author("stephb").
+
+%% API
+-export([]).
+
+-callback set_configuration( Configuration :: #{} ) -> ok | { error, Reason::term() }.
+-callback pause( all | [UUID::string()]) -> ok | { error, Reason::term() }.
+-callback start( all | [UUID::string()]) -> ok | { error, Reason::term() }.
+-callback stop( all | [UUID::string()]) -> ok | { error, Reason::term() }.
+-callback cancel( all | [UUID::string()]) -> ok | { error, Reason::term() }.

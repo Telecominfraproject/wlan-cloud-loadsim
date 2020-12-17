eb027f1b49267454652aee5566f67b1d33a20650
Stephane Bourque <stephane.bourque@gmail.com>
2020-12-16 17:36:53 -0800

    Fixing API

--- src/mqtt_client.erl
+++ src/mqtt_client.erl
@@ -37,15 +37,16 @@
 											start_time = 0 :: number(),
 											send_report_timer}).
 
--spec start(CAName::binary(),Id::binary(),Configuration::gen_configuration_b(), ManagerPid::pid()) -> no_return().
-start(CAName,Id,Configuration,ManagerPid)->
+-spec start(CAName::binary(),Serial::binary(),Configuration::gen_configuration_b(), ManagerPid::pid()) -> no_return().
+start(CAName,Serial,Configuration,ManagerPid)->
+	io:format("STARTING CLIENT: ~p~n",[Serial]),
 	#{ <<"broker">> := Broker, <<"compress">> := Compress, <<"port">> := Port, <<"topics">> := Topics } = Configuration,
 	%% io:format(">>>>Trying topic: ~p  connect to: ~p:~p~n",[Topics,Broker,Port]),
 	NewConfig = #{ broker => <<"opensync-mqtt-broker.wlan.local">>, compress => Compress,
 	            port => list_to_integer(binary_to_list(Port)), topics => Topics },
-	{ok,DeviceConfiguration} = inventory:get_client(CAName,Id),
+	{ok,DeviceConfiguration} = inventory:get_client(CAName,Serial),
 	full_start(#client_state{
-		id = Id,
+		id = Serial,
 		caname = CAName,
 		manager_pid = ManagerPid,
 		details = DeviceConfiguration,

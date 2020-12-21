40fb6e4c4fe83ab0ca27b946d748fa34836f9a46
helge <helge@syramo.com>
2020-12-20 19:29:20 -0800

    created simop branch + added somop module

--- src/ovsdb_ap_config.erl
+++ src/ovsdb_ap_config.erl
@@ -46,13 +46,11 @@ new (CAName,Id,Store,Redirector) ->
 -spec configure (Config :: cfg()) -> NewConfig :: cfg().
 configure (#cfg{ca_name=CAName, id=ID, redirector=R}=Config) ->
 	{ok,Info} = inventory:get_client(CAName,ID),
-	SSID = case Info#client_info.wifi_clients of
-		[{_,_,_,S,_}|_] ->
-			S;
-		_ ->
-			<<"TipWlan-cloud-wifi">>
-	end,
-
+	SSIDs = #{'BAND2G' => <<"TipWlan-cloud-wifi">>,
+			  'BAND5G' => <<"TipWlan-cloud-wifi">>,
+			  'BAND5GL' => <<"Maverick">>,
+			  'BAND5GU' => <<"Maverick">>},
+	WifiClients = lists:flatten(Info#client_info.wifi_clients),
 	HW = case hardware:get_by_id(Info#client_info.id) of
 		{ok,[HardwareInfo]} ->
 			HardwareInfo;
@@ -68,21 +66,16 @@ configure (#cfg{ca_name=CAName, id=ID, redirector=R}=Config) ->
 		{lan_addr,<<"192.168.1.1">>},
 		{lan_mac,Info#client_info.lan_mac0},
 		{tip_redirector,R},
-		{wifi_clients,get_all_wifi_macs(Info#client_info.wifi_clients)},
+		{wifi_clients,WifiClients},
 		{name,Info#client_info.name},
-		{ssid,SSID},
 		{bands,Info#client_info.bands},
-		{hardware,HW}
-
-		% {serial,<<"21P10C69717951">>},
-		% {type,<<"EA8300">>},
-		% {wan_addr,<<"10.20.0.113">>},
-		% {wan_mac,<<"58:ef:68:62:e7:f1">>},
-		% {lan_addr,<<"192.168.1.1">>},
-		% {lan_mac,<<"58:ef:68:62:e7:f0">>},
-		% {tip_redirector,<<"ssl:opensync-controller.wlan.local:6643">>}
+		{hardware,HW},
+		{ssids,SSIDs}
 	],
-	initialize_ap_tables(Config#cfg.store_ref,validate_config(APC)),
+	APC2 = validate_config(APC),
+	ovsdb_ap_simop:create_ap_node(APC2,Config#cfg.store_ref),
+	ovsdb_ap_simop:create_radios(APC2,Config#cfg.store_ref),
+	ovsdb_ap_simop:create_clients(APC2,Config#cfg.store_ref),
 	Config#cfg{
 		cacert = Info#client_info.cacert,
 		serial = Info#client_info.serial,
@@ -113,21 +106,6 @@ make_ip_addr(_ID) ->
 	D = rand:uniform(230) + 10,
 	list_to_binary(io_lib:format("~B.~B.~B.~B",[A,B,C,D])).
 
--spec initialize_ap_tables (Store :: ets:tid(), Config :: proplists:proplist()) -> true.
-initialize_ap_tables (Store, APC) ->
-	create_radio_tables(APC,Store),
-	create_VIF_tables(APC,Store),
-	create_table('AWLAN_Node',APC,Store),
-%	create_table('Wifi_Radio_Config',APC,Store),
-%	create_table('Wifi_Radio_State',APC,Store),
-	create_table('Wifi_Inet_Config',APC,Store),
-	create_table('Wifi_Inet_State',APC,Store),
-%	create_table('Wifi_RRM_Config',APC,Store),
-%	create_table('Wifi_Stats_Config',APC,Store),
-	create_table('DHCP_leased_IP',APC,Store),
-%	create_table('Wifi_VIF_Config',APC,Store),
-%	create_table('Wifi_VIF_State',APC,Store),
-	create_table('Wifi_Associated_Clients',APC,Store).
 	
 %%------------------------------------------------------------------------------
 %% accessor API - direct config settings
@@ -185,273 +163,5 @@ get_host_or_port (Part, Addr) when is_binary(Addr) ->
 						  _ -> 0
 				end
 	end.
-
-create_radio_tables(APC,Store)->
-	lists:foldl(fun(E,N)->
-								Band = convert_band(E),
-								RadioConfigUUID = utils:uuid_b(),
-								Wifi_RRM_ConfigUUID = utils:uuid_b(),
-								IFName = << <<"radio">>/binary , ($0+N) >>,
-								Wifi_Stats_ConfigUUID = utils:uuid_b(),
-								ets:insert(Store, #'Wifi_Stats_Config'{
-									'**key_id**' = Wifi_Stats_ConfigUUID,
-									'_uuid' = [<<"uuid">>,Wifi_Stats_ConfigUUID],
-									radio_type = Band}),
-								ets:insert(Store,#'Wifi_RRM_Config'{
-									'**key_id**' = Wifi_RRM_ConfigUUID,
-									'_uuid' = [<<"uuid">>,Wifi_RRM_ConfigUUID],
-									'_version' = [<<"uuid">>,utils:uuid_b()],
-									freq_band = Band,
-									min_load = 40,
-									backup_channel = get_backup_channel(Band),
-									snr_percentage_drop = 30
-								}),
-								ets:insert(Store, #'Wifi_Radio_Config'{
-							    '**key_id**' = RadioConfigUUID,
-							    '_uuid' = [<<"uuid">>, RadioConfigUUID],
-							    '_version' = [<<"uuid">>,utils:uuid_b()],
-							    freq_band = Band,
-							    if_name = IFName
-								}),
-		            ets:insert(Store, #'Wifi_Radio_State'{
-									'**key_id**' = utils:uuid_b(),
-									'_version' = [<<"uuid">>,utils:uuid_b()],
-									if_name = IFName,
-									mac = modify_mac(proplists:get_value(wan_mac,APC),0),
-									bcn_int = 100,
-									allowed_channels = [<<"set">>, get_allowed_channels(Band)],
-									radio_config = [<<"uuid">>,RadioConfigUUID],
-									vif_states = [<<"set">>,[]], % [<<"uuid">>,<<"87f75538-67d0-408a-9c8b-018665754d48">>],
-									country = <<"US">>,
-									radar = [<<"map">>,[]],
-									tx_chainmask = 3,
-									channel = get_default_channel(Band),
-									tx_power = 18,
-									ht_mode = <<"HT80">>,
-									hw_mode = get_hw_mode(Band),
-									enabled = true,
-									freq_band = Band
-								}), N+1
-		end,0,proplists:get_value(bands,APC)).
-
-create_VIF_tables(APC,Store)->
-	Wifi_VIF_ConfigUUID = utils:uuid_b(),
-	ets:insert(Store,#'Wifi_VIF_Config'{
-		'**key_id**' = Wifi_VIF_ConfigUUID,
-		ssid = proplists:get_value(ssid,APC),
-		'_version' = [<<"uuid">>,utils:uuid_b()]
-	}),
-	ets:insert(Store,#'Wifi_VIF_State'{
-		'**key_id**' = utils:uuid_b(),
-		mac = proplists:get_value(wan_mac,APC),
-		associated_clients = [<<"set">>,proplists:get_value(wifi_clients,APC)],
-		vif_config = [<<"uuid">>,Wifi_VIF_ConfigUUID],
-		ssid = proplists:get_value(ssid,APC),
-		'_version' = [<<"uuid">>,utils:uuid_b()]
-	}).
-
-create_table ('Wifi_Inet_State',APC,Store) ->
-	ets:insert(Store, #'Wifi_Inet_State'{
-		'**key_id**' = utils:uuid_b(),
-		if_name= <<"wwan">>,
-		if_type = <<"eth">>,
-		enabled = false,
-		'_version' = [<<"uuid">>,<<"0b10958d-9bfb-45e5-9c36-ad8327750607">>],
-		inet_config = [<<"uuid">>,<<"7e38a63b-526a-4b83-b30e-edd4c17ab3f6">>]
-	}),
-	ets:insert(Store, #'Wifi_Inet_State'{
-		'**key_id**' = utils:uuid_b(),
-		dhcpd = [<<"map">>,[[<<"lease_time">>,<<"12h">>],[<<"start">>,<<"100">>],[<<"stop">>,<<"150">>]]],
-		if_name= <<"lan">>,
-		if_type = <<"bridge">>,
-		enabled = true,
-		netmask = <<"255.255.255.0">>,
-		inet_addr = proplists:get_value(lan_addr,APC),
-		'_version' = [<<"uuid">>,<<"6237745e-3a4d-41a3-858d-7cbce39f5b8c">>],
-		hwaddr = proplists:get_value(wan_mac,APC),
-		network = true,
-		mtu = 1500,
-		ip_assign_scheme = <<"static">>,
-		inet_config = [<<"uuid">>,<<"19484645-8519-4bd0-98dd-13f1fec83395">>]
-	}),
-	ets:insert(Store, #'Wifi_Inet_State'{
-		'**key_id**' = utils:uuid_b(),
-		if_name= <<"wan6">>,
-		if_type = <<"eth">>,
-		enabled = false,
-		'_version' = [<<"uuid">>,<<"ac171d81-5e5f-41a9-aa71-44a11bb2f72b">>],
-		inet_config = [<<"uuid">>,<<"b803af39-e392-437b-8c86-dd87d24f8b49">>]
-	}),
-	ets:insert(Store, #'Wifi_Inet_State'{
-		'**key_id**' = utils:uuid_b(),
-		if_name= <<"wan">>,
-		if_type = <<"bridge">>,
-		enabled = true,
-		netmask = <<"255.255.255.0">>,
-		'NAT' = true,
-		inet_addr = proplists:get_value(wan_addr,APC),
-		'_version' = [<<"uuid">>,<<"325acfc1-ca59-4cbe-8316-7ed307663881">>],
-		hwaddr = proplists:get_value(wan_mac,APC),
-		network = true,
-		mtu = 1500,
-		dns = [<<"map">>,[[<<"primary">>,<<"10.20.0.1">>]]],
-		ip_assign_scheme = <<"dhcp">>,
-		gateway = <<"10.20.0.1">>,
-		inet_config = [<<"uuid">>,<<"1a533ecc-90d7-499e-a76c-0d593a446fdb">>]
-	});
-
-create_table ('Wifi_Inet_Config',APC,Store) ->
-	UUID1 = utils:uuid_b(),
-	ets:insert(Store, #'Wifi_Inet_Config'{
-		'**key_id**' = UUID1,
-		'_uuid' = [<<"uuid">>, UUID1],
-		dhcpd = [<<"map">>,[]],
-		if_name = <<"wan">>,
-		mtu = [<<"set">>,[]],
-		network = true,
-		dns = [<<"map">>,[]],
-		if_type = <<"bridge">>,
-		broadcast = [<<"set">>,[]],
-		enabled = true,
-		vlan_id = [<<"set">>,[]],
-		netmask = [<<"set">>,[]],
-		gateway = [<<"set">>,[]],
-		'NAT' = true,
-		ip_assign_scheme = <<"dhcp">>,
-		inet_addr = [<<"set">>,[]]
-	}),
-	UUID2 = utils:uuid_b(),
-	ets:insert(Store, #'Wifi_Inet_Config'{
-		'**key_id**' = UUID2,
-		'_uuid' = [<<"uuid">>, UUID2],
-		if_name = <<"wan6">>,
-		network = true,
-		if_type = <<"bridge">>,
-		enabled = true,
-		'NAT' = false
-	}),
-	UUID3 = utils:uuid_b(),
-	ets:insert(Store, #'Wifi_Inet_Config'{
-		'**key_id**' = UUID3,
-		'_uuid' = [<<"uuid">>, UUID3],
-		if_name = <<"wwan">>,
-		network = true,
-		if_type = <<"eth">>,
-		enabled = true,
-		'NAT' = false,
-		ip_assign_scheme = <<"dhcp">>
-	}),
-	UUID4 = utils:uuid_b(),
-	ets:insert(Store, #'Wifi_Inet_Config'{
-		'**key_id**' = UUID4,
-		'_uuid' = [<<"uuid">>, UUID4],
-		dhcpd = [<<"map">>,[[<<"lease_time">>,<<"12h">>],[<<"start">>,<<"100">>],[<<"stop">>,<<"150">>]]],
-		if_name = <<"lan">>,
-		network = true,
-		if_type = <<"bridge">>,
-		enabled = true,
-		netmask = <<"255.255.255.0">>,
-		'NAT' = false,
-		ip_assign_scheme = <<"static">>,
-		inet_addr = proplists:get_value(lan_addr,APC)
-	});
-
-create_table ('Wifi_Associated_Clients',APC,Store) ->
-	%io:format("CONFIGURED WIFI CLIENTS:~n~p~n",[proplists:get_value(wifi_clients,APC)]),
-	lists:foldl( fun(MAC,A)->
-									ets:insert(Store, #'Wifi_Associated_Clients'{
-										'**key_id**' = utils:uuid_b(),
-										'_version' = [<<"uuid">>, utils:uuid_b()],
-										mac = MAC,
-										state = <<"active">>
-									}), A
-								end,[],proplists:get_value(wifi_clients,APC));
-	%%end,
-	%%[F(X) || X <- proplists:get_value(wifi_clients,APC)];
-	% ets:insert(Store, #'Wifi_Associated_Clients'{
-	% 	'**key_id**' = <<"ee49ed4e-5a04-4100-bf6a-ebfbbc54250e">>,
-	% 	'_version' = [<<"uuid">>,<<"5bc3eb0f-1cc3-4dae-aae5-af02c8d2f1c7">>],
-	% 	mac = <<"52:b6:76:03:6d:f2">>,
-	% 	state = <<"active">>,
-	% 	uapsd = [<<"set">>,[]],
-	% 	capabilities = [<<"set">>,[]],
-	% 	kick = [<<"map">>,[]],
-	% 	oftag = [<<"set">>,[]]
-	% });
-
-create_table ('DHCP_leased_IP',APC,Store) ->
-	NM = proplists:get_value(name,APC),
-	lists:foldl( fun(MAC,N)->
-										Vendor = case oui_server:lookup_oui_from_mac(MAC) of
-											         {ok,X} -> X;
-															 _ -> <<"unknown">>
-										         end,
-
-		ets:insert(Store, #'DHCP_leased_IP'{
-										'**key_id**' = utils:uuid_b(),
-										'_version' = [<<"uuid">>, utils:uuid_b()],
-										hostname = iolist_to_binary([proplists:get_value(name,APC),"_",integer_to_list(N)]),
-										inet_addr = iolist_to_binary(["192.168.1.",integer_to_list(N+1)]),
-										hwaddr = MAC,
-										vendor_class = Vendor,
-										device_name = iolist_to_binary([NM,".SimClient_",integer_to_list(N+1)])
-									}),
-									N+1
-								end,1,proplists:get_value(wifi_clients,APC));
-
-
-create_table ('AWLAN_Node',APC,Store) ->
-	HW = proplists:get_value(hardware,APC),
-	ets:insert(Store, #'AWLAN_Node'{
-		'**key_id**' = utils:uuid_b(),
-		redirector_addr = proplists:get_value(tip_redirector,APC),
-		serial_number = proplists:get_value(serial,APC),
-		id = proplists:get_value(serial,APC),
-		model = proplists:get_value(type,APC),
-		revision = <<"1">>,
-		platform_version = <<"OPENWRT_EA8300">>,
-		firmware_version = <<"0.1.0">>,
-		version_matrix = [<<"map">>,[
-							[<<"DATE">>,HW#hardware_info.firmware_date],
-							[<<"FIRMWARE">>,HW#hardware_info.firmware],
-							[<<"FW_BUILD">>,HW#hardware_info.firmware_build],
-							[<<"FW_COMMIT">>,HW#hardware_info.firmware_commit],
-							[<<"FW_IMAGE_ACTIVE">>,HW#hardware_info.firmware],
-							[<<"FW_IMAGE_INACTIVE">>,HW#hardware_info.firmware_image_inactive],
-							[<<"FW_PROFILE">>,HW#hardware_info.firmware_profile],
-							[<<"FW_VERSION">>,HW#hardware_info.firmware_version],
-							[<<"HOST">>,HW#hardware_info.firmware_host],
-							[<<"OPENSYNC">>,HW#hardware_info.opensync],
-							[<<"core">>,HW#hardware_info.core],
-							[<<"vendor/tip">>,HW#hardware_info.vendor_tip]
-						 ]]
-	}).
-
-get_all_wifi_macs(Clients)->
-	Fl = lists:flatten(Clients),
-	[MAC || {_,_,_,MAC,_} <- Fl].
 	
-convert_band('BAND2G')-> <<"2.4G">>;
-convert_band('BAND5GL')-> <<"5GL">>;
-convert_band('BAND5GU')-> <<"5GU">>.
-
-modify_mac(MAC,N) ->
-	[X1,X2,$:,X3,X4,$:,X5,X6,$:,X7,X8,$:,X9,X10,$:,X11,_X12] = binary_to_list(MAC),
-	list_to_binary([X1,X2,$:,X3,X4,$:,X5,X6,$:,X7,X8,$:,X9,X10,$:,X11,N+$0]).
-
-get_allowed_channels(<<"5GU">>)->[100,104,108,112,116,120,124,128,132,136,140,144,149,153,154,157,161,165];
-get_allowed_channels(<<"2.4G">>)->[1,2,3,4,5,6,7,8,9,10,11];
-get_allowed_channels(<<"5GL">>)->[36,40,44,48,52,56,60,64].
-
-get_default_channel(<<"5GU">>)->149;
-get_default_channel(<<"2.4G">>)->6;
-get_default_channel(<<"5GL">>)->36.
-
-get_backup_channel(<<"5GU">>)->154;
-get_backup_channel(<<"2.4G">>)->11;
-get_backup_channel(<<"5GL">>)->44.
-
-get_hw_mode(<<"5GU">>)-><<"11ac">>;
-get_hw_mode(<<"2.4G">>)-><<"11n">>;
-get_hw_mode(<<"5GL">>)-><<"11ac">>.
 
---
+++ src/ovsdb_ap_simop.erl
@@ -0,0 +1,317 @@
+%%%-----------------------------------------------------------------------------
+%%% @author helge
+%%% @copyright (C) 2020, Arilia Wireless Inc.
+%%% @doc
+%%% 
+%%% @end
+%%% Created : 20. December 2020 @ 09:19:42
+%%%-----------------------------------------------------------------------------
+-module(ovsdb_ap_simop).
+-author("helge").
+
+-include("../include/common.hrl").
+-include("../include/ovsdb_ap_tables.hrl").
+-include("../include/inventory.hrl").
+
+-export ([create_ap_node/2,create_radios/2,create_clients/2]).
+
+
+
+%%-----------------------------------------------------------------------------
+%% managing the access point
+%%-----------------------------------------------------------------------------
+
+-spec create_ap_node(APC :: proplists:proplist(), Store :: ets:tid()) -> ok.
+create_ap_node(APC,Store) ->
+    create_AWLAN_Node(APC,Store),
+    create_AWLAN_Inet(APC,Store).
+
+create_AWLAN_Node (APC,Store) ->
+	HW = proplists:get_value(hardware,APC),
+	ets:insert(Store, #'AWLAN_Node'{
+		'**key_id**' = utils:uuid_b(),
+		redirector_addr = proplists:get_value(tip_redirector,APC),
+		serial_number = proplists:get_value(serial,APC),
+		id = proplists:get_value(serial,APC),
+		model = proplists:get_value(type,APC),
+		revision = <<"1">>,
+		platform_version = <<"OPENWRT_EA8300">>,
+		firmware_version = <<"0.1.0">>,
+		version_matrix = [<<"map">>,[
+							[<<"DATE">>,HW#hardware_info.firmware_date],
+							[<<"FIRMWARE">>,HW#hardware_info.firmware],
+							[<<"FW_BUILD">>,HW#hardware_info.firmware_build],
+							[<<"FW_COMMIT">>,HW#hardware_info.firmware_commit],
+							[<<"FW_IMAGE_ACTIVE">>,HW#hardware_info.firmware],
+							[<<"FW_IMAGE_INACTIVE">>,HW#hardware_info.firmware_image_inactive],
+							[<<"FW_PROFILE">>,HW#hardware_info.firmware_profile],
+							[<<"FW_VERSION">>,HW#hardware_info.firmware_version],
+							[<<"HOST">>,HW#hardware_info.firmware_host],
+							[<<"OPENSYNC">>,HW#hardware_info.opensync],
+							[<<"core">>,HW#hardware_info.core],
+							[<<"vendor/tip">>,HW#hardware_info.vendor_tip]
+						 ]]
+	}).
+
+create_AWLAN_Inet (APC,Store) ->
+    UUID1 = utils:uuid_b(),
+	ets:insert(Store, #'Wifi_Inet_Config'{
+		'**key_id**' = UUID1,
+		'_uuid' = [<<"uuid">>, UUID1],
+		dhcpd = [<<"map">>,[]],
+		if_name = <<"wan">>,
+		mtu = [<<"set">>,[]],
+		network = true,
+		dns = [<<"map">>,[]],
+		if_type = <<"bridge">>,
+		broadcast = [<<"set">>,[]],
+		enabled = true,
+		vlan_id = [<<"set">>,[]],
+		netmask = [<<"set">>,[]],
+		gateway = [<<"set">>,[]],
+		'NAT' = true,
+		ip_assign_scheme = <<"dhcp">>,
+		inet_addr = [<<"set">>,[]]
+	}),
+    ets:insert(Store, #'Wifi_Inet_State'{
+		'**key_id**' = utils:uuid_b(),
+		if_name= <<"wwan">>,
+		if_type = <<"eth">>,
+		enabled = false,
+		'_version' = [<<"uuid">>,<<"0b10958d-9bfb-45e5-9c36-ad8327750607">>],
+		inet_config = [<<"uuid">>,UUID1]
+	}),
+    UUID2 = utils:uuid_b(),
+	ets:insert(Store, #'Wifi_Inet_Config'{
+		'**key_id**' = UUID2,
+		'_uuid' = [<<"uuid">>, UUID2],
+		if_name = <<"wan6">>,
+		network = true,
+		if_type = <<"bridge">>,
+		enabled = true,
+		'NAT' = false
+	}),
+    ets:insert(Store, #'Wifi_Inet_State'{
+		'**key_id**' = utils:uuid_b(),
+		dhcpd = [<<"map">>,[[<<"lease_time">>,<<"12h">>],[<<"start">>,<<"100">>],[<<"stop">>,<<"150">>]]],
+		if_name= <<"lan">>,
+		if_type = <<"bridge">>,
+		enabled = true,
+		netmask = <<"255.255.255.0">>,
+		inet_addr = proplists:get_value(lan_addr,APC),
+		'_version' = [<<"uuid">>,<<"6237745e-3a4d-41a3-858d-7cbce39f5b8c">>],
+		hwaddr = proplists:get_value(wan_mac,APC),
+		network = true,
+		mtu = 1500,
+		ip_assign_scheme = <<"static">>,
+		inet_config = [<<"uuid">>,UUID2]
+	}),
+    UUID3 = utils:uuid_b(),
+	ets:insert(Store, #'Wifi_Inet_Config'{
+		'**key_id**' = UUID3,
+		'_uuid' = [<<"uuid">>, UUID3],
+		if_name = <<"wwan">>,
+		network = true,
+		if_type = <<"eth">>,
+		enabled = true,
+		'NAT' = false,
+		ip_assign_scheme = <<"dhcp">>
+	}),
+    ets:insert(Store, #'Wifi_Inet_State'{
+		'**key_id**' = utils:uuid_b(),
+		if_name= <<"wan6">>,
+		if_type = <<"eth">>,
+		enabled = false,
+		'_version' = [<<"uuid">>,<<"ac171d81-5e5f-41a9-aa71-44a11bb2f72b">>],
+		inet_config = [<<"uuid">>,UUID3]
+	}),
+    UUID4 = utils:uuid_b(),
+	ets:insert(Store, #'Wifi_Inet_Config'{
+		'**key_id**' = UUID4,
+		'_uuid' = [<<"uuid">>, UUID4],
+		dhcpd = [<<"map">>,[[<<"lease_time">>,<<"12h">>],[<<"start">>,<<"100">>],[<<"stop">>,<<"150">>]]],
+		if_name = <<"lan">>,
+		network = true,
+		if_type = <<"bridge">>,
+		enabled = true,
+		netmask = <<"255.255.255.0">>,
+		'NAT' = false,
+		ip_assign_scheme = <<"static">>,
+		inet_addr = proplists:get_value(lan_addr,APC)
+	}),
+    ets:insert(Store, #'Wifi_Inet_State'{
+		'**key_id**' = utils:uuid_b(),
+		if_name= <<"wan">>,
+		if_type = <<"bridge">>,
+		enabled = true,
+		netmask = <<"255.255.255.0">>,
+		'NAT' = true,
+		inet_addr = proplists:get_value(wan_addr,APC),
+		'_version' = [<<"uuid">>,<<"325acfc1-ca59-4cbe-8316-7ed307663881">>],
+		hwaddr = proplists:get_value(wan_mac,APC),
+		network = true,
+		mtu = 1500,
+		dns = [<<"map">>,[[<<"primary">>,<<"10.20.0.1">>]]],
+		ip_assign_scheme = <<"dhcp">>,
+		gateway = <<"10.20.0.1">>,
+		inet_config = [<<"uuid">>,UUID4]
+	}).
+	
+
+%%-----------------------------------------------------------------------------
+%% managing radios for the AP
+%%-----------------------------------------------------------------------------
+
+-spec create_radios (APC :: proplists:proplist(), Store :: ets:tid()) -> ok.
+create_radios(APC,Store) ->
+    lists:foldl(fun(Band,N)->create_radio(Band,N,APC,Store), N+1 end,
+                0,
+                proplists:get_value(bands,APC)),
+    create_initial_VIF_config(APC,Store).
+
+-spec create_radio (Band :: atom(), N :: integer(), APC :: proplists:proplist(), Store :: ets:tid()) -> ok.
+create_radio(B,N,APC,Store) ->
+    Band = band_map(B),
+    RadioConfigUUID = utils:uuid_b(),
+    Wifi_RRM_ConfigUUID = utils:uuid_b(),
+    IFName = << <<"radio">>/binary , ($0+N) >>,
+    Wifi_Stats_ConfigUUID = utils:uuid_b(),
+    DefChannel = get_default_channel(Band),
+    BackupChannel = get_backup_channel(Band,DefChannel),
+    ets:insert(Store, #'Wifi_Stats_Config'{
+        '**key_id**' = Wifi_Stats_ConfigUUID,
+        '_uuid' = [<<"uuid">>,Wifi_Stats_ConfigUUID],
+        radio_type = Band}),
+    ets:insert(Store,#'Wifi_RRM_Config'{
+        '**key_id**' = Wifi_RRM_ConfigUUID,
+        '_uuid' = [<<"uuid">>,Wifi_RRM_ConfigUUID],
+        '_version' = [<<"uuid">>,utils:uuid_b()],
+        freq_band = Band,
+        min_load = 40,
+        backup_channel = BackupChannel,
+        snr_percentage_drop = 30
+    }),
+    ets:insert(Store, #'Wifi_Radio_Config'{
+        '**key_id**' = RadioConfigUUID,
+        '_uuid' = [<<"uuid">>, RadioConfigUUID],
+        '_version' = [<<"uuid">>,utils:uuid_b()],
+        freq_band = Band,
+        if_name = IFName
+    }),
+    ets:insert(Store, #'Wifi_Radio_State'{
+        '**key_id**' = utils:uuid_b(),
+        '_version' = [<<"uuid">>,utils:uuid_b()],
+        if_name = IFName,
+        mac = modify_mac(proplists:get_value(wan_mac,APC),0),
+        bcn_int = 100,
+        allowed_channels = [<<"set">>, get_allowed_channels(Band)],
+        radio_config = [<<"uuid">>,RadioConfigUUID],
+        vif_states = [<<"set">>,[]], % [<<"uuid">>,<<"87f75538-67d0-408a-9c8b-018665754d48">>],
+        country = <<"US">>,
+        radar = [<<"map">>,[]],
+        tx_chainmask = 3,
+        channel = DefChannel,
+        tx_power = 18,
+        ht_mode = <<"HT80">>,
+        hw_mode = get_hw_mode(Band),
+        enabled = true,
+        freq_band = Band
+    }).
+
+-spec create_initial_VIF_config (APC :: proplists:proplist(), Store :: ets:tid()) -> ok.
+create_initial_VIF_config (APC,Store) ->
+    Wifi_VIF_ConfigUUID = utils:uuid_b(),
+    #{'BAND2G' := SSID} = proplists:get_value(ssids,APC),
+    MACs = [ MAC || {_,_,_,MAC,_} <- proplists:get_value(wifi_clients,APC)],
+	ets:insert(Store,#'Wifi_VIF_Config'{
+		'**key_id**' = Wifi_VIF_ConfigUUID,
+		ssid = SSID,
+		'_version' = [<<"uuid">>,utils:uuid_b()]
+	}),
+	ets:insert(Store,#'Wifi_VIF_State'{
+		'**key_id**' = utils:uuid_b(),
+		mac = proplists:get_value(wan_mac,APC),
+		associated_clients = [<<"set">>,MACs],
+		vif_config = [<<"uuid">>,Wifi_VIF_ConfigUUID],
+		ssid = SSID,
+		'_version' = [<<"uuid">>,utils:uuid_b()]
+	}).
+
+%%-----------------------------------------------------------------------------
+%% managing clients
+%%-----------------------------------------------------------------------------
+
+-spec create_clients(APC :: proplists:proplist(), Store :: ets:tid()) -> ok.
+create_clients(APC,Store) ->
+    create_ap_wifi_clients(APC,Store),
+    create_ap_lan_clients(APC,Store).
+
+-spec create_ap_lan_clients(APC :: proplists:proplist(), Store :: ets:tid()) -> ok.
+create_ap_lan_clients (_APC,_Store) ->
+    ok.
+
+-spec create_ap_wifi_clients (APC :: proplists:proplist(), Store :: ets:tid()) -> ok.
+create_ap_wifi_clients (APC,Store) ->
+    % Cl = proplists:get_value(wifi_clients,APC),
+    % io:format("Wifi Clients:~n~p~n",[Cl]).
+    [ create_wifi_client(Cl,APC,Store) || Cl <- proplists:get_value(wifi_clients,APC)].
+    
+create_wifi_client ({Idx,_Band,_SSID,MAC,Vendor},APC,Store) ->
+    NM = proplists:get_value(name,APC),
+    % VendorClass = case oui_server:lookup_oui_from_mac(MAC) of
+    %         {ok,X} -> X;
+    %         _ -> <<"unknown">>
+    % end,
+    ets:insert(Store, #'Wifi_Associated_Clients'{
+        '**key_id**' = utils:uuid_b(),
+        '_version' = [<<"uuid">>, utils:uuid_b()],
+        mac = MAC,
+        state = <<"active">>
+    }),
+    ets:insert(Store, #'DHCP_leased_IP'{
+        '**key_id**' = utils:uuid_b(),
+        '_version' = [<<"uuid">>, utils:uuid_b()],
+        hostname = iolist_to_binary(["H_",NM,"_",integer_to_list(Idx)]),
+        inet_addr = iolist_to_binary(["192.168.1.",integer_to_list(Idx+1)]),
+        hwaddr = MAC,
+        vendor_class = Vendor,
+        device_name = iolist_to_binary([NM,".SimClient_",integer_to_list(Idx+1)])
+    }).
+
+
+
+%%-----------------------------------------------------------------------------
+%% helper functions
+%%-----------------------------------------------------------------------------
+
+-spec band_map(atom()) -> binary().
+band_map('BAND2G') -> <<"2.4G">>;
+band_map('BAND5G') -> <<"5GL">>;
+band_map('BAND5GL') -> <<"5GL">>;
+band_map('BAND5GU') -> <<"5GU">>;
+band_map(_) -> <<"2.4G">>.
+
+get_allowed_channels(<<"5GU">>)->[100,104,108,112,116,120,124,128,132,136,140,144,149,153,154,157,161,165];
+get_allowed_channels(<<"2.4G">>)->[1,2,3,4,5,6,7,8,9,10,11];
+get_allowed_channels(<<"5GL">>)->[36,40,44,48,52,56,60,64].
+
+get_default_channel(Band) ->
+    Allowed = list_to_tuple(get_allowed_channels(Band)),
+    element(rand:uniform(size(Allowed)),Allowed).
+
+get_backup_channel(Band,N) ->
+    case get_default_channel(Band) of
+        N ->
+            get_backup_channel(Band,N) ;
+        Ch ->
+            Ch
+    end.
+    
+modify_mac(MAC,N) ->
+	[X1,X2,$:,X3,X4,$:,X5,X6,$:,X7,X8,$:,X9,X10,$:,X11,_X12] = binary_to_list(MAC),
+	list_to_binary([X1,X2,$:,X3,X4,$:,X5,X6,$:,X7,X8,$:,X9,X10,$:,X11,N+$0]).
+
+get_hw_mode(<<"5GU">>)-><<"11ac">>;
+get_hw_mode(<<"2.4G">>)-><<"11n">>;
+get_hw_mode(<<"5GL">>)-><<"11ac">>.
+

6286595df865d1647611fa1920baba882350a9dd
stephb9959 <stephane.bourque@gmail.com>
2021-01-25 13:40:55 -0800

    Docker fixes

--- docker_run_manager.sh
+++ docker_run_manager.sh
@@ -1,3 +1,3 @@
 mkdir docker_logs_manager
 # docker run -it -p 9091:9090 --init --volume="$PWD/ssl:/etc/ssl/certs" --volume="$PWD/docker_logs:/app_data/logs" -e ERL_NODE_NAME="simmanager1@renegademac.arilia.com" tip-owls-1
-docker run -it -p 9091:9090 --init --volume="$PWD/ssl:/etc/ssl/certs" --volume="$PWD/docker_logs_manager:/app_data/logs" --network=owls -e ERL_NODE_NAME="simmanager1@renegademac.arilia.com" tip-owls-manager
+docker run -d -p 9091:9090 --init --volume="$PWD/ssl:/etc/ssl/certs" --volume="$PWD/docker_logs_manager:/app_data/logs" -e ERL_NODE_NAME="simmanager1@renegademac.arilia.com" -e ERL_OPTIONS="-noshell -noinput" tip-owls-manager

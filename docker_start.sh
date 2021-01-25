00d3c83ae8f8ae97e8502000cd9ef85f4dc37594
stephb9959 <stephane.bourque@gmail.com>
2021-01-25 12:46:11 -0800

    fixing stats

--- docker_start.sh
+++ docker_start.sh
@@ -2,4 +2,4 @@
 
 # This file in invoked from within docker to start the application. It should not be used otherwise.
 cd /owls
-erl -config config/sys.config -args_file config/vm.args -name $ERL_NODE_NAME -detached -pa deps/*/ebin -pa ebin
+erl -config config/sys.config -args_file config/vm.args -name $ERL_NODE_NAME -pa deps/*/ebin -pa ebin
